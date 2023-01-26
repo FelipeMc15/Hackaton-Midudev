5037ff5abf0cb9f56a37cfafadff391a86892ca1
Lucas-FullStackX <lucasleguizamo21@gmail.com>
2023-01-25 19:28:59 -0500

    add config mantine ui

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,47 @@
-import { Html, Head, Main, NextScript } from "next/document";
-import { Link } from "next";
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+import {
+  createGetInitialProps,
+  createStylesServer,
+  ServerStyles,
+} from "@mantine/next";
+import Document, {
+  Head,
+  Html,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+
+const getInitialProps = createGetInitialProps();
+// optional: you can provide your cache as a first argument in createStylesServer function
+const stylesServer = createStylesServer();
+export default class _Document extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    // Add your app specific logic here
+
+    return {
+      ...initialProps,
+      styles: [
+        initialProps.styles,
+        <ServerStyles
+          html={initialProps.html}
+          server={stylesServer}
+          key="styles"
+        />,
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
